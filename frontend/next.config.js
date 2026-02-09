83e9b4c12102e44e8596c5b61ce519695f6647d1
sunil <sunil.kumar.mocharla@walmart.com>
2026-02-08 20:49:55 -0600

    Fix: add ESPN image domains for team logo loading

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,7 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ['i.ytimg.com', 'img.youtube.com'],
+    domains: [
+      'i.ytimg.com', 
+      'img.youtube.com',
+      'a.espncdn.com',
+      'media.api.espn.com',
+      'espncdn.com'
+    ],
   },
   eslint: {
     // We're using img tags in some places for performance reasons, these warnings can be ignored
